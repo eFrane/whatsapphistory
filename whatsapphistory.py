ae81e4baa8d772d5d62bb65b4e2629830c8bd7f3
Stefan Graupner <stefan.graupner@gmail.com>
2011-02-16 23:10:52 +0100

    parsing line data

--- whatsapphistory.py
+++ whatsapphistory.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#  WhatsApp History
+#  Copyright (C) 2010, Stefan Graupner
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, sys, re, zipfile, tempfile
 
 # get folder or zip file
@@ -15,15 +31,17 @@ else:
   dest=""
 
 # check for zip and unpack into temp folder
-if base.find(".zip", len(base)-5) > 0:
-  print "Extracting Zip File."
-  isZip = True
-  temp = tempfile.mkdtemp()
-  zip = zipfile.ZipFile(base)
-  zip.extractall(temp)
-  zip.close()
-else:
-  temp = base
+# if base.find(".zip", len(base)-5) > 0:
+#   print "Extracting Zip File."
+#   isZip = True
+#   temp = tempfile.mkdtemp()
+#   zip = zipfile.ZipFile(base)
+#   zip.extractall(temp)
+#   zip.close()
+# else:
+#   temp = base
+
+temp = os.getcwd()
 
 # find the chatlog and parse it line-by-line
 dirlist = os.listdir(temp)
@@ -33,7 +51,15 @@ for file in dirlist:
     lines = log.readlines()
     log.close()
     for line in lines:
-      print line
+      lineData = re.split('^([0-9/]*) ([0-9:]+ (?:AM|PM)): ([a-zA-Z ]+):(.*\n)', line, re.DOTALL)
+      # lineData[0]: empty
+      # lineData[1]: date
+      # lineData[2]: timestamp
+      # lineData[3]: author
+      # lineData[4]: message
+      # lineData[5]: empty
+
+      print lineData
 
 # clean temp folder
 #if isZip:
