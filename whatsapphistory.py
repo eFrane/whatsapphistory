04add21213ea465fbd8fb88ea272b770ce03725a
Stefan Graupner <stefan.graupner@gmail.com>
2011-02-17 09:38:11 +0100

    added pathname check on destination, zip temp clean.

--- whatsapphistory.py
+++ whatsapphistory.py
@@ -21,27 +21,30 @@ import os, sys, re, zipfile, tempfile, datetime, time
 if len(sys.argv) >= 2:
   base=sys.argv[1]
 else:
-  print "Usage: ./whatsapphistory.py <whatsappfolderorzipfile> [destinationfolder]",
+  print 'Usage: ./whatsapphistory.py <whatsappfolderorzipfile> [destinationfolder]'
   sys.exit()
 
 # determine destination
 if len(sys.argv) == 3:
-  dest=sys.argv[2]
+  dest = os.path.realpath(sys.argv[2])
+  if os.path.exists(dest) == False:
+    os.makedirs(dest)
 else:
-  dest=""
+  dest = os.getcwd()
 
 # check for zip and unpack into temp folder
-# if base.find(".zip", len(base)-5) > 0:
-#   print "Extracting Zip File."
-#   isZip = True
-#   temp = tempfile.mkdtemp()
-#   zip = zipfile.ZipFile(base)
-#   zip.extractall(temp)
-#   zip.close()
-# else:
-#   temp = base
+isZip = False
+if base.find('.zip', len(base)-5) > 0:
+  print 'Extracting Zip File.'
+  isZip = True
+  temp = tempfile.mkdtemp()
+  zip = zipfile.ZipFile(base)
+  zip.extractall(temp)
+  zip.close()
+else:
+  temp = base
 
-temp = os.getcwd()
+print 'Generating refurbished WhatsApp History in',dest
 
 # find the chatlog and parse it line-by-line
 dirlist = os.listdir(temp)
@@ -75,9 +78,11 @@ for file in dirlist:
 
 
 # clean temp folder
-#if isZip:
-#  for f in os.listdir(temp):
-#    fp = os.path.join(temp, f)
-#    try:
-#      if os.path.isfile(fp):
-#          os.unlink(fp)
+if isZip:
+  for f in os.listdir(temp):
+    fp = os.path.join(temp, f)
+    try:
+      if os.path.isfile(fp):
+          os.unlink(fp)
+    except:
+      print 'error.'
